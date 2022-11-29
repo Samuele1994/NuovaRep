08b455aafcd9d8f3614298a8b1ca851534fd1570
samuele secci <samusecciok54@gmail.com>
2022-11-29 10:50:14 +0100

    modifica del 29-11

--- resources/views/contatti.blade.php
+++ resources/views/contatti.blade.php
@@ -4,6 +4,27 @@
 
 @section('content')
 
-    <h1>{{$titolo}}</h1>
+    <div class="container mt-5">
+        <h1 class="mb-5">{{$titolo}}</h1>
+
+        <div class="container">
+            <div class="row">
+                <div class="col-md-6 mx-auto">
+
+                    <form action="">
+                        <div class="row">
+                            <div class="col">
+                                <label for="nome">Nome</label>
+                                <input class="form-control" type="text" name="nome" id="nome">
+                            </div>
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    </div>
+
+
 
 @endsection
